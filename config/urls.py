7a5fbf6d221f98c56028e60df0b3f6bf9aa5b51d
SaravanaKumar <jsaravanakumar2004@gmail.com>
2026-02-02 15:03:34 +0530

    Remove admin URL routes since admin app is disabled

--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
 
@@ -23,6 +22,6 @@ def home(request):
 
 urlpatterns = [
     path('', home),
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls), # Admin disabled
     path('api/', include('api.urls')),
 ]
